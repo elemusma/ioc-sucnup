9814efc96bdb34781ae8c59ce319855e439c5558
Efrain Lemus <efraintlemusm@gmail.com>
2023-01-11 11:38:47 -0700

    test connection

--- themes/insideoutcreative/page.php
+++ themes/insideoutcreative/page.php
@@ -4,7 +4,7 @@ if ( ! post_password_required( $post ) ) {
 
 if(get_the_content()){
 
-echo '<section class="pt-5 pb-5">';
+echo '<section class="pt-5 pb-5 s">';
 echo '<div class="container">';
 echo '<div class="row">';
 echo '<div class="col-md-12">';
